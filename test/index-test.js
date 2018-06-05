70ed7ddbb22a93f7daa7767d9b1b9425981bb7de
Randall Robinson <keith_adams007@yahoo.com>
2018-06-05 16:21:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,11 +4,5 @@ const jsdom = require('mocha-jsdom')
 const path = require('path')
 
 describe('index', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
 
-  it('runs', () => {
-    expect(true).toEqual(true)
-  })
 })
